f78fcf3c1ebd7d1104100a7a3c944f985409d037
shazkhan2 <seewardak@hotmail.com>
2023-12-29 00:49:47 +0100

    meals part slight tweaks

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,58 +1,35 @@
-const knex = require("knex")({
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "password1",
-    database: process.env.DB_NAME || "hyf_node_week3_warmup",
-    multipleStatements: true,
-  },
-});
-
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
+const router = express.Router();
+const path = require("path");
 
+const mealsRouter = require("./api/meals");
+const buildPath = path.join(__dirname, "../../dist");
+const port = process.env.PORT || 3000;
+const cors = require("cors");
+
+// For week4 no need to look into this!
+// Serve the built client html
+app.use(express.static(buildPath));
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
+// Parse JSON bodies (as sent by API clients)
 app.use(express.json());
 
-const apiRouter = express.Router();
-app.use("/api", apiRouter);
+app.use(cors());
 
-const contactsAPIRouter = express.Router();
-apiRouter.use("/contacts", contactsAPIRouter);
+router.use("/meals", mealsRouter);
 
+if (process.env.API_PATH) {
+  app.use(process.env.API_PATH, router);
+} else {
+  throw "API_PATH is not set. Remember to set it in your .env file"
+}
 
-// URL with injections for test http://localhost:3000/api/contacts?sort=last_name;%20DROP%20TABLE%20contacts;%20DESC
-
-contactsAPIRouter.get("/", async (req, res) => {
-  let query = knex.select("*").from("contacts");
-
-  if ("sort" in req.query) {
-      const orderBy = req.query.sort.toString();
-      if (orderBy.length > 0) {
-          const [column, order] = orderBy.split(' ');
-          if (column && order && (order.toUpperCase() === 'ASC' || order.toUpperCase() === 'DESC')) {
-              query = query.orderBy(column, order);
-          } else {
-              res.status(400).json({ error: "Invalid sort parameter" });
-              return;
-          }
-      }
-  }
-
-  const sql = query.toSQL().toNative();
-  console.log("SQL", sql);
-
-  try {
-      const data = await query;
-      res.json({ data });
-  } catch (e) {
-      console.error(e);
-      res.status(500).json({ error: e.message });
-  }
+// for the frontend. Will first be covered in the react class
+app.use("*", (req, res) => {
+  res.sendFile(path.join(`${buildPath}/index.html`));
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+module.exports = app;
