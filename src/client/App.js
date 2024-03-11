1d5dc57cff58f9d6b3ecdbf9b469a924c233aed9
shazkhan2 <seewardak@hotmail.com>
2024-03-11 05:23:26 +0100

    css path fixed

--- src/client/App.js
+++ src/client/App.js
@@ -1,19 +1,19 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import TestComponent from "./components/TestComponent/TestComponent";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import MealsList from "./components/MealsList";
+import Header from "./components/Header";
 
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <p>test</p>
-      </Route>
-      <Route exact path="/lol">
-        <p>lol</p>
-      </Route>
-      <Route exact path="/test-component">
-        <TestComponent></TestComponent>
-      </Route>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <div className="home-page">
+            <h1>Meal Sharing App</h1>
+          </div>
+        </Route>
+        <Route exact path="/meals" component={MealsList} />
+      </Switch>
     </Router>
   );
 }
